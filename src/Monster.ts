4a01472ddad434bf06cce0aed8470b10bd437234
Diego Silva <coxcorp@gmail.com>
2022-05-02 18:01:45 -0300

    requisito 09

---
+++ src/Monster.ts
@@ -0,0 +1,36 @@
+import { SimpleFighter } from './Fighter';
+
+class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  public get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  public get strength(): number {
+    return this._strength;
+  }
+
+  public receiveDamage(attackPoints: number): number {
+    const damage = this._lifePoints - attackPoints;
+    const remainLifePoints = this._lifePoints - attackPoints;
+    if (remainLifePoints <= 0) {
+      this._lifePoints = -1;
+    } else if (damage > 0) {
+      this._lifePoints = remainLifePoints;
+    }
+    return this._lifePoints;
+  }
+
+  public attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+}
+
+export default Monster;

2f62f7e9683f677ee9147488b6af6d7af0612695
Diego Silva <coxcorp@gmail.com>
2022-05-07 23:37:43 -0300

    requisito 11

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,51 @@
+import Fighter, { SimpleFighter } from '../Fighter';
+import Battle from './Battle';
+
+class PVE extends Battle {
+  fighter: Fighter;
+  monsters: SimpleFighter[];
+
+  constructor(figther: Fighter, monsters: SimpleFighter[]) {
+    super(figther);
+    this.fighter = figther;
+    this.monsters = monsters;
+  }
+
+  isAlive(): boolean {
+    if (this.fighter.lifePoints > 0 
+      && this.monsters.some((element) => element.lifePoints > 0)) {
+      return true;
+    }
+    return false;
+  }
+
+  figtherWins(): void {
+    const creatures = this.monsters.find(
+      (element) => element.lifePoints > 0,
+    );
+    if (creatures) {
+      this.fighter.attack(creatures);
+    }
+  }
+  
+  monstersWins(): void {
+    this.monsters.forEach((e) => {
+      if (e.lifePoints > 0) {
+        e.attack(this.fighter);
+      }
+    });
+  }
+
+  fight(): number {
+    while (this.isAlive()) {
+      this.figtherWins();      
+      this.monstersWins();
+    }
+    if (this.fighter.lifePoints < 0) {
+      return -1;
+    }
+    return 1;
+  }
+}
+
+export default PVE;
